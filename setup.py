58b3d50faca1fe657459732b33e511aaf88b80d6
lairuilin <kalai850531@g.ncu.edu.tw>
2020-07-20 10:26:58 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Created on 21-06-2020 12:32:28
+"""
+__author__ = "lairuilin"
+__email__ = "kalai850531@g.ncu.edu.tw"
+__status__ = "Development"
+
+import setuptools
+
+setup(name='loggerTest',
+      version='0.0.1',
+      description='string to date',
+      url='',
+      author='ruilin',
+      author_email='kalai850531@g.ncu.edu.tw',
+      license='MIT',
+      packages=['loggerTest'])
